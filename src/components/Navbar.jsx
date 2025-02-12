efd470f0e97d6927ab42e03532b8166b21d1e223
Alicoder001 <coderkuchkarov@gmail.com>
2025-02-12 11:10:08 +0500

    Navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,20 @@
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="bg-[#315B2D] w-full">
+      <div className="container">
+        <button className="flex gap-2 px-16 py-[1px] rounded-2xl bg-white items-center">
+          <span>2025 honorees</span>
+          <img
+            height={37}
+            width={37}
+            src="/public/assets/images/icons/arrow-left.svg"
+            alt="Arrow left"
+          />
+        </button>
+        <button>Craft Day Sign Up</button>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
