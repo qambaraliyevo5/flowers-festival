a9dd9910d7282c538364b4643cdee14d9331030b
Alicoder001 <coderkuchkarov@gmail.com>
2025-02-12 11:44:48 +0500

    React router dom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,22 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <div className="bg-[#315B2D] w-full">
       <div className="container">
-        <button className="flex gap-2 px-16 py-[1px] rounded-2xl bg-white items-center">
-          <span>2025 honorees</span>
-          <img
-            height={37}
-            width={37}
-            src="/public/assets/images/icons/arrow-left.svg"
-            alt="Arrow left"
-          />
-        </button>
+        <Link>
+          <button className="flex gap-2 px-16 py-[1px] rounded-3xl bg-white items-center">
+            <span className="text-[#1B7B58] tracking-[3.75px] text-sm">
+              2025 honorees
+            </span>
+            <img
+              height={37}
+              width={37}
+              src="/public/assets/images/icons/arrow-left.svg"
+              alt="Arrow left"
+            />
+          </button>
+        </Link>
         <button>Craft Day Sign Up</button>
       </div>
     </div>
